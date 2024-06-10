c6039d5a6d1fca76438d8cd326933ab069b1bb9b
Simone Romagnoli <simone.romagnoli@bioinfolab.usl4.toscana.it>
2024-06-10 14:38:17 +0200

    Fixed typos

--- workflow/scripts/DESeqFromFeatureCounts.STAR.R
+++ workflow/scripts/DESeqFromFeatureCounts.STAR.R
@@ -744,7 +744,7 @@ heatmap.2(norm_counts_DEG_mat,
 dev.off()
 
 #Heatmap DEG res_
-res_filter_rlog <- merge(as.data.frame(res_filter), as.data.frame(counts(dds, normalized=TRUE)), by="row.names", sort=FALSE)
+res_filter_rlog <- merge(as.data.frame(res_filter), as.data.frame(assay(rlog)), by="row.names", sort=FALSE)
 norm_counts_DEG <- res_filter_rlog[c(1,14:ncol(res_filter_rlog))]
 rownames(norm_counts_DEG) <- norm_counts_DEG$Row.names
 norm_counts_DEG$Row.names <- NULL
@@ -1058,7 +1058,7 @@ dev.off()
 
 #Heatmap DEG rlog
 resNorm_filter_rlog <- merge(as.data.frame(resNorm_filter), as.data.frame(assay(rlog)), by="row.names", sort=FALSE)
-norm_counts_DEG <- resNorm_filter_vst[c(1,14:ncol(resNorm_filter_vst))]
+norm_counts_DEG <- resNorm_filter_rlog[c(1,14:ncol(resNorm_filter_rlog))]
 rownames(norm_counts_DEG) <- norm_counts_DEG$Row.names
 norm_counts_DEG$Row.names <- NULL
 
@@ -1199,6 +1199,41 @@ geneTree = as.dendrogram(hr, method="average")
 #     ylab = "Height")
 
 pdf(file.path(opt$output,"heatmap_Ash_DEG.vst.pdf"),as.numeric(opt$Height), width = as.numeric(opt$width))
+nrow_heat <- nrow(norm_counts_DEG_mat)
+heatmap.2(norm_counts_DEG_mat,
+          Rowv=as.dendrogram(hr), 
+          Colv=as.dendrogram(hc),
+          col=redgreen(100),
+          scale="row",
+          margins = c(7, 7),
+          cexCol = 0.7,
+          labRow = F,
+          main = paste0("DEG Ash heatmap n=", nrow_heat),
+          trace = "none")
+dev.off()
+
+#Heatmap DEG Ash rlog
+resAsh_filter_rlog <- merge(as.data.frame(resAsh_filter), as.data.frame(assay(rlog)), by="row.names", sort=FALSE)
+norm_counts_DEG <- resAsh_filter_rlog[c(1,14:ncol(resAsh_filter_rlog))]
+rownames(norm_counts_DEG) <- norm_counts_DEG$Row.names
+norm_counts_DEG$Row.names <- NULL
+
+norm_counts_DEG_mat <- as.matrix(norm_counts_DEG)
+scaledata <- t(scale(t(norm_counts_DEG_mat)))
+hc <- hclust(as.dist(1-cor(scaledata, method="spearman")), method="complete")
+sampleTree = as.dendrogram(hc, method="average")
+#plot(sampleTree,
+#     main = "Sample Clustering",
+#     ylab = "Height")
+
+hr <- hclust(as.dist(1-cor(t(scaledata), method="pearson")), method="complete") # Cluster rows by Pearson correlation.
+geneTree = as.dendrogram(hr, method="average")
+#plot(geneTree,
+#     leaflab = "none",             
+#     main = "Gene Clustering",
+#     ylab = "Height")
+
+pdf(file.path(opt$output,"heatmap_Ash_DEG.rlog.pdf"),as.numeric(opt$Height), width = as.numeric(opt$width))
 nrow_heat <- nrow(norm_counts_DEG_mat)
 heatmap.2(norm_counts_DEG_mat,
           Rowv=as.dendrogram(hr), 
--- workflow/scripts/DESeqFromHTSeq.STAR.R
+++ workflow/scripts/DESeqFromHTSeq.STAR.R
@@ -245,7 +245,7 @@ ggsave(pca_rlog, filename = file.path(opt$output,"2DPCA.rlog.top1k.pdf"),height=
 ## PCA PCAplot
 ## Generate p
 rlog.output <- assay(rlog)
-p <- pca(rlog.output, metadata = coldata, removeVar = 0.1)
+p <- pca(rlog.output, metadata = coldata, removeVar = NULL)
 #Determine optimum number of PCs to retain
 #Let's perform Horn's parallel analysis 
 horn <- parallelPCA(rlog.output)
@@ -311,7 +311,7 @@ ggsave(pca_vst, filename = file.path(opt$output,"2DPCA.vst.top1k.pdf"),height=as
 ## PCA PCAplot
 ## Generate p
 vst.output <- assay(vst)
-p <- pca(vst.output, metadata = coldata, removeVar = 0.1)
+p <- pca(vst.output, metadata = coldata, removeVar = NULL)
 #Determine optimum number of PCs to retain
 #Let's perform Horn's parallel analysis 
 horn <- parallelPCA(vst.output)
@@ -674,6 +674,12 @@ write.table(res_, file.path(opt$output, "results.txt"), row.names = FALSE, quote
 #write.xlsx(res_, file.path(opt$output,"results.xlsx"),row.names=FALSE)
 write.csv(res_, file.path(opt$output,"results.csv"), row.names = FALSE, quote = FALSE)
 
+#Export the filtered results
+res_filter <- res_[(which(res_$pvalue <= 0.05 & abs(res_$log2FoldChange)>=0.58)),]
+write.table(res_filter, file.path(opt$output, "results.filtered.txt"), row.names = FALSE, quote = FALSE, sep = "\t")
+#write.xlsx(res_filter, file.path(opt$output,"results.filtered.xlsx"),row.names=FALSE)
+write.csv(res_filter, file.path(opt$output,"results.filtered.csv"), row.names = FALSE, quote = FALSE)
+
 #Heatmap DEG res_
 res_filter_norm <- merge(as.data.frame(res_filter), as.data.frame(counts(dds, normalized=TRUE)), by="row.names", sort=FALSE)
 norm_counts_DEG <- res_filter_norm[c(1,14:ncol(res_filter_norm))]
@@ -745,7 +751,7 @@ heatmap.2(norm_counts_DEG_mat,
 dev.off()
 
 #Heatmap DEG res_
-res_filter_rlog <- merge(as.data.frame(res_filter), as.data.frame(counts(dds, normalized=TRUE)), by="row.names", sort=FALSE)
+res_filter_rlog <- merge(as.data.frame(res_filter), as.data.frame(assay(rlog)), by="row.names", sort=FALSE)
 norm_counts_DEG <- res_filter_rlog[c(1,14:ncol(res_filter_rlog))]
 rownames(norm_counts_DEG) <- norm_counts_DEG$Row.names
 norm_counts_DEG$Row.names <- NULL
@@ -1059,7 +1065,7 @@ dev.off()
 
 #Heatmap DEG rlog
 resNorm_filter_rlog <- merge(as.data.frame(resNorm_filter), as.data.frame(assay(rlog)), by="row.names", sort=FALSE)
-norm_counts_DEG <- resNorm_filter_vst[c(1,14:ncol(resNorm_filter_vst))]
+norm_counts_DEG <- resNorm_filter_rlog[c(1,14:ncol(resNorm_filter_rlog))]
 rownames(norm_counts_DEG) <- norm_counts_DEG$Row.names
 norm_counts_DEG$Row.names <- NULL
 
@@ -1200,6 +1206,41 @@ geneTree = as.dendrogram(hr, method="average")
 #     ylab = "Height")
 
 pdf(file.path(opt$output,"heatmap_Ash_DEG.vst.pdf"),as.numeric(opt$Height), width = as.numeric(opt$width))
+nrow_heat <- nrow(norm_counts_DEG_mat)
+heatmap.2(norm_counts_DEG_mat,
+          Rowv=as.dendrogram(hr), 
+          Colv=as.dendrogram(hc),
+          col=redgreen(100),
+          scale="row",
+          margins = c(7, 7),
+          cexCol = 0.7,
+          labRow = F,
+          main = paste0("DEG Ash heatmap n=", nrow_heat),
+          trace = "none")
+dev.off()
+
+#Heatmap DEG Ash rlog
+resAsh_filter_rlog <- merge(as.data.frame(resAsh_filter), as.data.frame(assay(rlog)), by="row.names", sort=FALSE)
+norm_counts_DEG <- resAsh_filter_rlog[c(1,14:ncol(resAsh_filter_rlog))]
+rownames(norm_counts_DEG) <- norm_counts_DEG$Row.names
+norm_counts_DEG$Row.names <- NULL
+
+norm_counts_DEG_mat <- as.matrix(norm_counts_DEG)
+scaledata <- t(scale(t(norm_counts_DEG_mat)))
+hc <- hclust(as.dist(1-cor(scaledata, method="spearman")), method="complete")
+sampleTree = as.dendrogram(hc, method="average")
+#plot(sampleTree,
+#     main = "Sample Clustering",
+#     ylab = "Height")
+
+hr <- hclust(as.dist(1-cor(t(scaledata), method="pearson")), method="complete") # Cluster rows by Pearson correlation.
+geneTree = as.dendrogram(hr, method="average")
+#plot(geneTree,
+#     leaflab = "none",             
+#     main = "Gene Clustering",
+#     ylab = "Height")
+
+pdf(file.path(opt$output,"heatmap_Ash_DEG.rlog.pdf"),as.numeric(opt$Height), width = as.numeric(opt$width))
 nrow_heat <- nrow(norm_counts_DEG_mat)
 heatmap.2(norm_counts_DEG_mat,
           Rowv=as.dendrogram(hr), 
